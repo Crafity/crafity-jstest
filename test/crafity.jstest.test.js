2b32af89dd798ead330b94edfcea076d72b447ba
Bart Riemens <briemens@crafity.com>
2013-07-11 13:23:49 +0200

    Added a new simple async test to expose a node v0.6 issue

--- test/crafity.jstest.test.js
+++ test/crafity.jstest.test.js
@@ -10,5 +10,15 @@ mainContext.run({
 		var test_jstest = jstest.createContext();
 		var result = test_jstest.run;
 		assert.isDefined(result, 'Expected a result');
-	}
+	},
+  "Run test a simple async test": function (context) {
+    context.async(3000);
+    
+    setTimeout(context.complete, 100);
+    
+    context.on("complete", function (err, result) {
+      assert.hasNoValue(err, "Didn't expect an error");
+      assert.areEqual([], result, "Expected another result");
+    });
+ 	}
 });
