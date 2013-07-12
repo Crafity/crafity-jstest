eabb7b4e1253bfae70167f4a883eb38dd1a68310
Bart Riemens <briemens@crafity.com>
2013-07-12 16:57:36 +0200

    Added a new test for the JSLint functionality

---
+++ test/crafity.jstest.jslint.test.js
@@ -0,0 +1,27 @@
+/*jslint node: true, bitwise: true, unparam: true, maxerr: 50, white: true */
+"use strict";
+
+/*!
+ * crafity-jstest - Test Framework for JavaScript
+ * Copyright(c) 2013 Crafity
+ * Copyright(c) 2013 Bart Riemens
+ * Copyright(c) 2013 Galina Slavova
+ * MIT Licensed
+ */
+
+/**
+ * Test dependencies.
+ */
+
+var jstest = require('../lib/crafity.jstest')
+  , testContext = jstest.createContext("JSLint tests")
+  , assert = jstest.assert
+  ;
+
+testContext.run({
+  "Intantiate the JSLint wrapper": function () {
+    
+    var jslint = require('../lib/crafity.jstest.jslint');
+    assert.isFunction(jslint.JSLinter, "Expected a JSLinter constructor");
+  }
+});
