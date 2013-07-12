3729765479756e85186c578d5d050502306b6ddd
Bart Riemens <briemens@crafity.com>
2013-07-12 16:54:16 +0200

    Tuned the JSLint code and files. Got it to a nice and compact state with JSLint auto updating

--- lib/vendor/updateJslint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm jslint.js* jslint.module.js
-wget -nv https://raw.github.com/douglascrockford/JSLint/master/jslint.js
-sed -e '/JSLINT here/r jslint.js' ./jslint.module.template.js > ./jslint.module.js
