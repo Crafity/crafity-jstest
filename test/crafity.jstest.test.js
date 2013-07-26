3a840430c359baaf519e69065fedab458c405fba
Bart Riemens <briemens@crafity.com>
2013-07-27 01:59:41 +0200

    Reformatted all the JS files

--- test/crafity.jstest.test.js
+++ test/crafity.jstest.test.js
@@ -19,19 +19,19 @@ var jstest = require('../lib/crafity.jstest')
   ;
 
 mainContext.run({
-	"Run test with no arguments must return a result": function () {
-		var test_jstest = jstest.createContext();
-		var result = test_jstest.run;
-		assert.isDefined(result, 'Expected a result');
-	},
+  "Run test with no arguments must return a result": function () {
+    var test_jstest = jstest.createContext();
+    var result = test_jstest.run;
+    assert.isDefined(result, 'Expected a result');
+  },
   "Run test a simple async test": function (context) {
     context.async(3000);
-    
+
     setTimeout(context.complete, 100);
-    
+
     context.on("complete", function (err, result) {
       assert.hasNoValue(err, "Didn't expect an error");
       assert.areEqual([], result, "Expected another result");
     });
- 	}
+  }
 });
